48724a47e13c5235e4b2239f8ff3cc88d6ae38c0
Marvin Klein <marvin.klein@harold-scholz.de>
2023-03-15 16:35:10 +0100

    Projektdateien hinzufügen.

---
+++ BlazorInputTags/InputTagOptions.cs
@@ -0,0 +1,53 @@
+﻿namespace BlazorInputTags
+{
+    public class InputTagOptions
+    {
+        /// <summary>
+        /// Gets or sets the name of the class for the tag wrapper
+        /// </summary>
+        public string WrapperClass { get; set; } = "blazor-tag-wrapper";
+        /// <summary>
+        /// Gets or sets the name of the class for the tag list
+        /// </summary>
+        public string TagListClass { get; set; } = "blazor-tag-list";
+        /// <summary>
+        /// Gets or sets the name of the class for a tag
+        /// </summary>
+        public string TagClass { get; set; } = "blazor-tag";
+        /// <summary>
+        /// Gets or sets the name of the class for the input field.
+        /// </summary>
+        public string InputClass { get; set; } = "blazor-tag-input";
+        /// <summary>
+        /// Gets or sets the name of the class for the label
+        /// </summary>
+        public string LabelClass { get; set; } = "blazor-tag-label";
+        /// <summary>
+        /// Gets or sets the text for the tooltip from the remove button
+        /// </summary>
+        public string RemoveButtonTooltip { get; set; } = "Remove";
+        /// <summary>
+        /// Gets or sets the placeholder text for the input field.
+        /// </summary>
+        public string InputPlaceholder { get; set; } = "Enter tag, add with Enter";
+        /// <summary>
+        /// Defines whether the component should show a label in above the input field.
+        /// </summary>
+        public bool DisplayLabel { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets the maximum length of a tag in order to be able to be added to the list.
+        /// <para>0 = No minimum length</para>
+        /// </summary>
+        public int MinLength { get; set; }
+        /// <summary>
+        /// Gets or sets the maximum length of a tag in order to be able to be added to the list.
+        /// <para>0 = No maximum length</para>
+        /// </summary>
+        public int MaxLength { get; set; }
+        /// <summary>
+        /// Function to validate the user input.
+        /// </summary>
+        public Func<string, Task<bool>>? ValidateTag { get; set; }
+    }
+}
---
+++ BlazorInputTags/InputTags.razor.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+
+namespace BlazorInputTags
+{
+    public partial class InputTags
+    {
+
+    }
+}
