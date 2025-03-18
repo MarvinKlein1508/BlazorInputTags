9fdd91fc499bb01c20854e2773d5cb531c48e524
MarvinKlein1508 <support@marvinkleinmusic.de>
2025-03-18 17:09:38 +0100

    List search results

---
+++ BlazorInputTags/OptionSearchEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorInputTags;
+
+public class OptionsSearchEventArgs<T>
+{
+    /// <summary>
+    /// Gets or sets the text to search.
+    /// </summary>
+    public string Text { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the list of items to display.
+    /// </summary>
+    public IEnumerable<T>? Items { get; set; }
+}
