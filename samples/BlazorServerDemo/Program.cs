4156e25099ee4925937249b82519316ea0dc6fbd
MarvinKlein1508 <support@marvinkleinmusic.de>
2025-03-19 13:43:57 +0100

    Update BlazorServerDemo

---
+++ samples/BlazorServerDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using BlazorServerDemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddAdditionalAssemblies([typeof(SamplesShared.IApplicationMarker).Assembly])
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ samples/SamplesShared/SampleData/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace SamplesShared.SampleData;
+
+public record Person(int PersonId, string CountryCode, string FirstName, string LastName, DateOnly BirthDate, string Picture)
+{
+    public override string ToString() => $"{FirstName} {LastName} ({BirthDate}, {CountryCode})";
+
+    public string GetName() => $"{FirstName} {LastName}";
+}
+
+public class SimplePerson
+{
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
