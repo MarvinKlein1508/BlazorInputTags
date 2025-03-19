33b592a352bc0c85dd5297a01d128feb7509d553
MarvinKlein1508 <support@marvinkleinmusic.de>
2025-03-19 09:39:03 +0100

    Initial keyboard support

---
+++ BlazorInputTags/GenericInputTags.razor.cs
@@ -0,0 +1,152 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+
+namespace BlazorInputTags
+{
+    public partial class GenericInputTags<TValue>
+    {
+        private readonly Guid _id = Guid.NewGuid();
+
+        private ElementReference? _reference;
+        private DotNetObjectReference<GenericInputTags<TValue>>? _dotNetHelper = null;
+        private IJSObjectReference Module { get; set; } = default!;
+        private bool _wasSetToEmpty;
+        private string _input = string.Empty;
+
+        private TValue? SelectedItem { get; set; }
+        public string Input
+        {
+            get => _input;
+            set
+            {
+                _wasSetToEmpty = value == string.Empty;
+                _input = value;
+            }
+        }
+
+        [JSInvokable]
+        public async Task OnKeyUpRaisedAsync()
+        {
+            Console.WriteLine("EVENT FIRED");
+        }
+
+        [JSInvokable]
+        public async Task OnItemSelectedAsync()
+        {
+            if (SelectedItem is null)
+            {
+                return;
+            }
+
+            if (Value.Contains(SelectedItem))
+            {
+                Value.Remove(SelectedItem);
+            }
+            else
+            {
+                Value.Add(SelectedItem);
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
+        [JSInvokable]
+        public async Task SelectNextItemAsync()
+        {
+            if (SelectedItem is null)
+            {
+                SelectedItem = _items.FirstOrDefault();
+            }
+            else
+            {
+                int currentIndex = _items.IndexOf(SelectedItem);
+
+                if (currentIndex is -1)
+                {
+                    SelectedItem = _items.FirstOrDefault();
+                }
+                else if (currentIndex + 1 < _items.Count)
+                {
+                    SelectedItem = _items[currentIndex + 1];
+                }
+            }
+
+
+            await InvokeAsync(StateHasChanged);
+        }
+        [JSInvokable]
+        public async Task SelectPreviousItemAsync()
+        {
+            if (SelectedItem is null)
+            {
+                SelectedItem = _items.FirstOrDefault();
+            }
+            else
+            {
+                int currentIndex = _items.IndexOf(SelectedItem);
+
+                if (currentIndex is -1)
+                {
+                    SelectedItem = _items.FirstOrDefault();
+                }
+                else if (currentIndex - 1 >= 0)
+                {
+                    SelectedItem = _items[currentIndex - 1];
+                }
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
+        [Parameter] public string Placeholder { get; set; } = string.Empty;
+        [Parameter] public List<TValue> Value { get; set; } = new List<TValue>();
+        [Parameter] public EventCallback<OptionsSearchEventArgs<TValue>> OnOptionsSearch { get; set; }
+        [Parameter] public RenderFragment<TValue>? ItemTemplate { get; set; }
+
+        private List<TValue> _items = [];
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Module = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BlazorInputTags/GenericInputTags.razor.js");
+                _dotNetHelper = DotNetObjectReference.Create(this);
+                await Module.InvokeVoidAsync("initialize", _id, _dotNetHelper);
+            }
+        }
+
+        private async Task InputHandlerAsync(ChangeEventArgs e)
+        {
+            Input = e.Value?.ToString() ?? string.Empty;
+
+            var args = new OptionsSearchEventArgs<TValue>()
+            {
+                Items = Array.Empty<TValue>(),
+                Text = Input,
+            };
+
+            await OnOptionsSearch.InvokeAsync(args);
+            _items = [.. args.Items];
+
+            SelectedItem = _items.FirstOrDefault();
+        }
+
+        private string GetSearchResultClass(TValue item)
+        {
+            bool valueContainsItem = Value.Contains(item);
+            if (valueContainsItem && item!.Equals(SelectedItem))
+            {
+                return "blazor-tag-active blazor-tag-selected";
+            }
+            else if (valueContainsItem)
+            {
+                return "blazor-tag-active";
+            }
+            else if (item!.Equals(SelectedItem))
+            {
+                return "blazor-tag-selected";
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ BlazorInputTags/GenericInputTags.razor.js
@@ -0,0 +1,21 @@
+﻿export function initialize(id, dotNetHelper) {
+    var element = document.getElementById(id);
+
+    element.addEventListener('keyup', function (e) {
+
+        if (e.key === "Enter") {
+            e.stopPropagation();
+            e.preventDefault();
+            dotNetHelper.invokeMethodAsync("OnItemSelectedAsync");
+        } else if (e.key == "ArrowDown") {
+            e.stopPropagation();
+            e.preventDefault();
+            dotNetHelper.invokeMethodAsync("SelectNextItemAsync");
+        } else if (e.key == "ArrowUp") {
+            e.stopPropagation();
+            e.preventDefault();
+            dotNetHelper.invokeMethodAsync("SelectPreviousItemAsync");
+        }
+
+    });
+}
